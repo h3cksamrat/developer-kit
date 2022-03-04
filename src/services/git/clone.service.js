35d25cda6ef016dad69bda96820f0ffa49bda063
windowSamrat <samratneupane215@gmail.com>
2022-03-05 00:32:02 +0545

    fixed error in git clone

--- src/services/git/clone.service.js
+++ src/services/git/clone.service.js
@@ -3,14 +3,19 @@ const { asyncTerminal } = require('../terminal');
 
 const clone = async (url, file_path) => {
   const command = `git -C ${file_path} clone ${url} --progress`;
-  const { stdout, stderr } = await asyncTerminal(command);
+  const { stdout, stderr, error } = await asyncTerminal(command);
+  let output;
+  if (!error) {
+    output = stdout.toString().split('\n');
+    return APIResponse('Completed');
+  }
   if (stderr) {
     const error = stderr.toString();
     console.error(error);
     return APIResponse(error, true);
   }
-  const output = stdout.toString();
-  return APIResponse(output);
+  output = stdout.toString().split('\n');
+  return APIResponse(output[0] + ' completed');
 };
 
 module.exports = clone;
