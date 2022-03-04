99ee36eb58dcbcbec07667038b3f25431a8949ca
windowSamrat <samratneupane215@gmail.com>
2022-03-05 00:19:49 +0545

    fixed error while space in between path

--- src/controllers/git.controller.js
+++ src/controllers/git.controller.js
@@ -1,6 +1,8 @@
 const { gitService } = require('../services');
+const { refactorPath } = require('../utils');
 
 const clone = async (folderPath, url) => {
+  folderPath = refactorPath(folderPath);
   const cloneResult = await gitService.clone(url, folderPath);
   return cloneResult;
 };
--- src/services/git/clone.service.js
+++ src/services/git/clone.service.js
@@ -1,8 +1,8 @@
 const { APIResponse } = require('../../utils');
 const { asyncTerminal } = require('../terminal');
 
-const clone = async(url, file_path) => {
-  const command = `git -C ${file_path} clone ${url}`;
+const clone = async (url, file_path) => {
+  const command = `git -C ${file_path} clone ${url} --progress`;
   const { stdout, stderr } = await asyncTerminal(command);
   if (stderr) {
     const error = stderr.toString();
