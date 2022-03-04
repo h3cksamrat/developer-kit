385fea39538d3973f66931d66b7a9fbb9bd37359
aayusshgh <aayusshgh@gmail.com>
2022-03-04 23:56:41 +0545

    nearly functional clonefunc

--- src/services/git/clone.service.js
+++ src/services/git/clone.service.js
@@ -1,12 +1,13 @@
 const { APIResponse } = require('../../utils');
 const { asyncTerminal } = require('../terminal');
 
-const clone = async(url, file_path) => {
+const clone = async (url, file_path) => {
+  console.log(url, file_path);
   const command = `git -C ${file_path} clone ${url}`;
   const { stdout, stderr } = await asyncTerminal(command);
   if (stderr) {
     const error = stderr.toString();
-    console.error(error);
+    console.error('error', error);
     return APIResponse(error, true);
   }
   const output = stdout.toString();
